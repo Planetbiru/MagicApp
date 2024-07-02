0ae918806ebd0f3d42a83e0ad41b54cfce9e33fc
Kamshory, MT <kamshory@alto.id>
2024-07-02 17:40:05 +0700

    Update UserAction.php

--- src/MagicApp/UserAction.php
+++ src/MagicApp/UserAction.php
@@ -4,17 +4,16 @@ namespace MagicApp;
 
 class UserAction
 {
-    const INSERT = "insert";
-    const CREATE = "create";
-    const UPDATE = "update";
-    const DELETE = "delete";
-    const ACTIVATE = "activate";
-    const DEACTIVATE = "deactivate";
-    const DETAIL = "detail";
-    const APPROVE = "approve";
-    const REJECT = "reject";
-    const SORT_ORDER = "sort_order";
-    
-    const SPECIAL_ACTION = "special_action";
-    const USER_ACTION = "user_action";
+    const INSERT            = "insert";
+    const CREATE            = "create";
+    const UPDATE            = "update";
+    const DELETE            = "delete";
+    const ACTIVATE          = "activate";
+    const DEACTIVATE        = "deactivate";
+    const DETAIL            = "detail";
+    const APPROVE           = "approve";
+    const REJECT            = "reject";
+    const SORT_ORDER        = "sort_order";  
+    const SPECIAL_ACTION    = "special_action";
+    const USER_ACTION       = "user_action";
 }
