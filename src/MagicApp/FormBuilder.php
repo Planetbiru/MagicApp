297b251325736b1913496562832c1382dea36e69
Kamshory, MT <kamshory@alto.id>
2024-07-02 17:45:56 +0700

    Update FormBuilder.php

--- src/MagicApp/FormBuilder.php
+++ src/MagicApp/FormBuilder.php
@@ -51,4 +51,28 @@ class FormBuilder
 
         return implode("\r\n", $htmlArray);
     }
+    
+    /**
+     * return selected="selected" if $param1 == $param2 
+     *
+     * @param mixed $param1
+     * @param mixed $param2
+     * @return string
+     */
+    public static function selected($param1, $param2)
+    {
+        return $param1 == $param2 ? ' selected="selected"' : '';
+    }
+    
+    /**
+     * return checked="checked" if $param1 == $param2 
+     *
+     * @param mixed $param1
+     * @param mixed $param2
+     * @return string
+     */
+    public static function checked($param1, $param2)
+    {
+        return $param1 == $param2 ? ' checked="checked"' : '';
+    }
 }
