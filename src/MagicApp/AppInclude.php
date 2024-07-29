8e84b0f38a7225ae59061d2893b64b28f45ae4be
Kamshory, MT <kamshory@yahoo.com>
2024-07-29 10:31:47 +0700

    Update

--- src/MagicApp/AppInclude.php
+++ src/MagicApp/AppInclude.php
@@ -88,7 +88,7 @@ class AppInclude
      */
     public function appForbiddenPage($dir)
     {
-        $path = $this->app->getBaseApplicationDirectory()."/".$this->app->getBaseIncludeDirectory()."/".$this->app->getForbiddenPage();
+        $path = $this->app->getBaseApplicationDirectory()."/".$this->app->getBaseIncludeDirectory()."/".$this->app->getForbiddenPage()."/403.php";
         if(PicoStringUtil::endsWith($path, ".php") && file_exists($path))
         {
             return $path;
@@ -108,11 +108,7 @@ class AppInclude
      */
     public function appNotFoundPage($dir)
     {
-        if(!isset($config))
-        {
-            $config = $this->appConfig;
-        }
-        $path = $config->getBaseIncludeDirectory()."/".$config->getInludeNotFoundFile();
+        $path = $this->app->getBaseApplicationDirectory()."/".$this->app->getBaseIncludeDirectory()."/".$this->app->getForbiddenPage()."/404.php";
         if($config != null && PicoStringUtil::endsWith($path, ".php") && file_exists($path))
         {
             return $path;
