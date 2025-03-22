de1aa52eccff89e70bc092952525515668b57ef8
Kamshory, MT <kamshory@yahoo.com>
2025-03-22 17:12:37 +0700

    Update MagicObject

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'planetbiru/magic-app',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '55d750d100df985fa52a18f290b1a73699135c0f',
+        'reference' => '4e155dfe0dd0eda274abc86b58672a1a63279cd6',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'planetbiru/magic-app' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '55d750d100df985fa52a18f290b1a73699135c0f',
+            'reference' => '4e155dfe0dd0eda274abc86b58672a1a63279cd6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'planetbiru/magic-object' => array(
             'pretty_version' => '3.8.0',
             'version' => '3.8.0.0',
-            'reference' => 'c403834dc7e8b8c708159ae002f744e59cf68b46',
+            'reference' => '2ad0d1367c93947e119eea8f25c0ea514f6ce701',
             'type' => 'library',
             'install_path' => __DIR__ . '/../planetbiru/magic-object',
             'aliases' => array(),
