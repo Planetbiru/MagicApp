feaaef0ac3e94e2278e3748ee931953598e03f98
Kamshory, MT <kamshory@alto.id>
2025-07-15 16:40:23 +0700

    Bug fix `isAllowedBatchAction`

--- src/AppUserPermission.php
+++ src/AppUserPermission.php
@@ -350,6 +350,10 @@ class AppUserPermission
      */
     public function isAllowedBatchAction()
     {
+        if(!$this->initialized)
+        {
+            $this->loadPermission();
+        }
         return $this->allowedBatchAction;
     }
 
